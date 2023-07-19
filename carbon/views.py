6c9973a82fd4af32e6b1ed24a5b8c65185b420ee
Collins07 <collinsabaya07@gmail.com>
2023-07-19 09:59:50 +0300

    carbon app

--- carbon/views.py
+++ carbon/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def carbon(request):
+    return render (request, 'carbon/index.html')
+
+def benefits(request):
+    return render (request, 'carbon/benefits.html')
